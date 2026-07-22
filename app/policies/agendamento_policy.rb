c5c2ef7a3c3212d745d3d3beed48041375929bc0
Danielle Zago <12zago.danielle@gmail.com>
2026-07-21 21:46:06 -0300

    feat(agendamento): cria AgendamentoPolicy
    destroy? restrito a admin, diferente das entidades de histórico
    médico que sempre bloqueiam destroy?.

---
+++ app/policies/agendamento_policy.rb
@@ -0,0 +1,27 @@
+class AgendamentoPolicy < ApplicationPolicy
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    true
+  end
+
+  def destroy?
+    user.admin?
+  end
+
+  class Scope < ApplicationPolicy::Scope
+    def resolve
+      scope.all
+    end
+  end
+end
